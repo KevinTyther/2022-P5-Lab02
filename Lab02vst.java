58df9cd997cc8d9c2c96954bfb7dc2ebc8cad478
Kevin <kevin.tyther@vtsd.com>
2022-09-14 13:04:04 -0400

    Lab

---
+++ Lab02vst.java
@@ -0,0 +1,21 @@
+public class Lab02vst
+{
+
+    public static void main(String args[])
+    {
+        System.out.println("Lab 02, 80 Point Version");
+        System.out.println();
+        System.out.println();
+        System.out.println("Measure today's knowledge ");
+        System.out.println("by yesterday's confusion.");
+        System.out.println();
+        System.out.println("Bewilderment + Exposure = Obvious");
+        System.out.println();
+        System.out.println("# ##### #####\t##### ##### # #   # ##### #####");
+        System.out.println("# #   # #   #\t#   # #   # # ##  #   #   #");
+        System.out.println("# #   # #   #\t##### #   # # # # #   #   #####");
+        System.out.println("# #   # #   #\t#     #   # # #  ##   #       #");
+        System.out.println("# ##### #####\t#     ##### # #   #   #   #####");
+    }
+
+}
